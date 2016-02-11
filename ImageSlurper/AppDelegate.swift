8e75bfb01d84efa7c23110337065359598a9b7e7
Stefan Buchholtz <stbuchholtz@gmail.com>
2016-02-11 18:49:34 +0100

    Initial Commit

---
+++ ImageSlurper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageSlurper
+//
+//  Created by Stefan Buchholtz on 11.02.16.
+//  Copyright © 2016 Stefan Buchholtz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
